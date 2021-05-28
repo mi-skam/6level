689d54c25875fa5d6c0e14c26b66f47e6d6047a0
mi-skam <maksim.codes@mailbox.org>
2021-05-28 22:26:34 +0200

    adding test file

--- lib_in_c/libl_test.c
+++ lib_in_c/libl_test.c
@@ -7,12 +7,12 @@ int main(int argc, char const *argv[])
 
     char *source;
     char *target;
+    source = ft_strnew(".");
+    target = ft_strnew(".");
 
-    source = ft_strnew(argv[1]);
-    target = ft_strnew("");
 
-
-    if (argc > 0) {
+    if (argc > 1) {
+        source = ft_strnew(argv[1]);
         ft_strcpy(target, source);
         printf("%s", target);
     }
---
+++ lib_in_c/test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+make clean && make all && ./libl_test it_works
